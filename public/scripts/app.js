5df1cfbbbbcec3386d08003091547e941d9a221c
Mich NS Wang <mich8588@gmail.com>
2021-10-04 17:07:14 +0800

    let Add option-button and removeAll-button work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,25 +10,43 @@ var app = {
 var onFormSubmit = function onFormSubmit(e) {
   //e為onSubmit事件
   e.preventDefault();
-  var option = e.target.elements.option.value = ''; //輸入後input區又變成空白(自動清除的概念)
+  var option = e.target.elements.option.value;
 
   if (option) {
     app.options.push(option); //將輸入的option加入options array內
 
+    console.log('push!');
     e.target.elements.option.value = ''; //輸入後input區又變成空白(自動清除的概念)
+
+    render();
+  } else {
+    console.log('error');
   }
 };
 
-var template =
-/*#__PURE__*/
-//use{}to introduce the content, that we can reuse the template
-//JSX expressions must have one parent element，所以使用<div>包裹其他標籤
-React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("h3", null, app.subtitle), app.options.length > 0 ? /*#__PURE__*/React.createElement("p", null, "Here are your options:", app.options.length) : /*#__PURE__*/React.createElement("p", null, "No options"), /*#__PURE__*/React.createElement("form", {
-  onSubmit: onFormSubmit
-}, /*#__PURE__*/React.createElement("input", {
-  type: "text",
-  name: "option",
-  placeholder: "option..."
-}), /*#__PURE__*/React.createElement("button", null, "Add Option")));
+var onRemoveAll = function onRemoveAll() {
+  app.options = [];
+  render();
+};
+
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+var render = function render() {
+  var template =
+  /*#__PURE__*/
+  //use{}to introduce the content, that we can reuse the template
+  //JSX expressions must have one parent element，所以使用<div>包裹其他標籤
+  React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("h3", null, app.subtitle), /*#__PURE__*/React.createElement("p", null, app.options.length > 0 ? 'Here are your options:' : 'No options'), /*#__PURE__*/React.createElement("p", null, app.options.length), /*#__PURE__*/React.createElement("button", {
+    type: "text",
+    name: "removeAll",
+    onClick: onRemoveAll
+  }, "Remove All"), /*#__PURE__*/React.createElement("form", {
+    onSubmit: onFormSubmit
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "option"
+  }), /*#__PURE__*/React.createElement("button", null, "Add Option")));
+  ReactDOM.render(template, appRoot);
+};
+
+render();
--- src/app.js
+++ src/app.js
@@ -7,29 +7,44 @@ const app={
 
 const onFormSubmit=(e)=>{ //e為onSubmit事件
     e.preventDefault();
-    const option=e.target.elements.option.value=''; //輸入後input區又變成空白(自動清除的概念)
+
+    const option=e.target.elements.option.value;
+
     if(option){
-        app.options.push(option) //將輸入的option加入options array內
+        app.options.push(option);//將輸入的option加入options array內
+        console.log('push!')
         e.target.elements.option.value='';//輸入後input區又變成空白(自動清除的概念)
+        render();
+    }else{
+        console.log('error')
     }
 }
 
-
-const template=(
-    //use{}to introduce the content, that we can reuse the template
-    //JSX expressions must have one parent element，所以使用<div>包裹其他標籤
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <h3>{app.subtitle}</h3>}
-        {app.options.length>0 ? <p>Here are your options:{app.options.length}</p>:<p>No options</p>}
-        <form onSubmit={onFormSubmit}>
-            <input type='text' name='option' placeholder='option...'></input>
-            <button>Add Option</button>
-        </form>
-    </div>
-);
+const onRemoveAll=()=>{
+    app.options=[]
+    render()
+}
 
 const appRoot=document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+const render=()=>{
+    const template=(
+        //use{}to introduce the content, that we can reuse the template
+        //JSX expressions must have one parent element，所以使用<div>包裹其他標籤
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <h3>{app.subtitle}</h3>}
+            <p>{app.options.length>0 ? 'Here are your options:':'No options'}</p>
+            <p>{app.options.length}</p>
+            <button type='text' name='removeAll' onClick={onRemoveAll}>Remove All</button>
+            <form onSubmit={onFormSubmit}>
+                <input type='text' name='option'></input>
+                <button>Add Option</button>
+            </form>
+            
+        </div>
+    );
+    ReactDOM.render(template, appRoot)
+}
 
+render();
